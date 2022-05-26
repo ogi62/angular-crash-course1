403adc6de01d9ab136ff23f09d8307b56d3075cd
Ogi62 <danguba962@gmail.com>
2022-05-26 19:06:52 +0200

    created task service,create and subscribe to a observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
   ],
   imports: [
     BrowserModule,
