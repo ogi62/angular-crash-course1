c4994ccee4b66305fa42e807edf11a66be68a999
Ogi62 <danguba962@gmail.com>
2022-06-22 22:19:32 +0200

    added db.json install json server make get request

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -9,11 +9,11 @@ import { TASKS } from 'src/app/mock-tasks';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/tasks';
+
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
