52355e93b1de4ee51d98d2b113c00e14269828b5
Ogi62 <danguba962@gmail.com>
2022-06-24 10:06:39 +0200

    made about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
 
 const appRoutes: Routes = [
   {path: '',component: TasksComponent}
@@ -24,6 +25,7 @@ const appRoutes: Routes = [
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
