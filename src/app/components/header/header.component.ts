82ede5cc81a5d74993862caad6dfa6e12cb7db95
Ognjen Dangubic <74620644+ogi62@users.noreply.github.com>
2022-06-24 09:52:17 +0200

    Toggle button (#7)
    * make ui.service and make toggleAddTask
    
    * make onToggle
    
    * make toggleAddTask
    
    * show add task change btn color and btn name

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -8,15 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title: string = 'Task Tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe((value)=> {
+      this.showAddTask = value;
+    });
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log("toggle");
+    this.uiService.toggleAddTask();
   }
 
 }
